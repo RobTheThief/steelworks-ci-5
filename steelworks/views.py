d39a247c9f1bc81991ba354048301a96294235b6
RobTheThief <cyberpeaches@gmail.com>
2022-10-30 21:46:17 +0000

    Hide django secret and create UserCreate,UserList views

--- steelworks/views.py
+++ steelworks/views.py
@@ -5,11 +5,11 @@ from rest_framework import generics
 from .serializers import UserSerializer
 
 
-class CustomerCreate(generics.CreateAPIView):
+class UserCreate(generics.CreateAPIView):
     queryset = User.objects.all(),
     serializer_class = UserSerializer
 
 
-class CustomerList(generics.ListAPIView):
+class UserList(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
