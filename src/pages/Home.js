666cb3f902a4d5c1e8eb0f9442397a5c343ff1bd
Rob Gannon <cyberpeaches@gmail.com>
2022-11-20 16:56:59 +0000

    Style:set min width for About heading and text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,12 +17,12 @@ function Home(props) {
         </div>
       </section>
       <section className="about-section mb-52 flex flex-col items-center justify-center" id="about">
-        <h2 className="lg:text-6xl md:text-4xl sm:text-3xl text-3xl w-1/2 mb-12 mt-64">
+        <h2 className={`lg:text-6xl md:text-4xl sm:text-3xl text-3xl w-1/2 mb-12 mt-64 ${css`min-width: 260px`}`}>
           <b>About</b>
           <br />
           Steelworks Fitness
         </h2>
-        <p className="w-1/2 m-2 text-xl">
+        <p className={`w-1/2 m-2 text-lg sm:text-xl ${css`min-width: 260px`}`}>
           "There is no limit to what you are capable of, physically and
           mentally. All trainers certified through nationally accredited
           organizations. Years of pushing our limits in performance have given
