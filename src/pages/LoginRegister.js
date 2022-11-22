26c1dd53a7ce63bcb316c6cf84c3104b5ad538ca
Rob Gannon <cyberpeaches@gmail.com>
2022-11-22 23:24:17 +0000

    Create Head for title & meta data component & add to LoginRegister page

--- src/pages/LoginRegister.js
+++ src/pages/LoginRegister.js
@@ -1,16 +1,23 @@
 import React, { useState } from "react";
+import Head from "../components/Head";
 import Login from "../components/Login";
 import Register from "../components/Register";
 
 export default function LoginRegister() {
-  const [isRegister, setIsRegister] = useState(false);
+  const [isRegister, setIsRegister] = useState(true);
   return (
-    <div className="w-full min-h-screen flex justify-center items-center login-register-page overflow-y-scroll">
-      {isRegister ? (
-        <Register setIsRegister={setIsRegister} />
-      ) : (
-        <Login setIsRegister={setIsRegister} />
-      )}
-    </div>
+    <>
+      <Head
+        title="Login/Register"
+        keywords="login, register, steelworks fitness, steelworks, fitness, gym"
+      />
+      <div className="w-full min-h-screen flex justify-center items-center login-register-page overflow-y-scroll">
+        {isRegister ? (
+          <Register setIsRegister={setIsRegister} />
+        ) : (
+          <Login setIsRegister={setIsRegister} />
+        )}
+      </div>
+    </>
   );
 }
