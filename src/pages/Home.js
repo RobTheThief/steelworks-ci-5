40a08fb30207faee46425ac78bb86ffd7dcd82d0
Rob Gannon <cyberpeaches@gmail.com>
2022-12-15 15:42:47 +0000

    Fix contact layout and copy.Add user review to about section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ import { css } from "@emotion/css";
 import { Button } from "@mui/material";
 import Modal from "../components/Modal";
 import { Helmet } from "react-helmet-async";
+import gabbyC from "../img/gabbyc.png";
 
 function Home() {
   const [showModal, setShowModal] = useState(false);
@@ -62,12 +63,41 @@ function Home() {
             min-width: 260px;
           `}`}
         >
-          "There is no limit to what you are capable of, physically and
-          mentally. All trainers certified through nationally accredited
-          organizations. Years of pushing our limits in performance have given
-          us an intutitive understanding of what it takes to get to a high level
-          os athleticism and strength, which we share with our clients."
+          There is no limit to what you are capable of, physically and mentally.
+          All trainers certified through nationally accredited organizations.
+          Years of pushing our limits in performance have given us an intutitive
+          understanding of what it takes to get to a high level of athleticism
+          and strength, which we share with our clients.
+          <br />
         </p>
+        <div
+          className={`w-1/2 m-2 text-lg sm:text-xl mt-8 ${css`
+            min-width: 260px;
+          `}`}
+        >
+          <p>
+            "I joined Steelworks gym a few months back on friend's
+            recommendation and I am so happy of my choice as this is the place
+            that I was looking for. Different from the other gyms that I joined
+            in the past which I left because of demotivation, Steelworks gym is
+            based on classes and this option is fantastic for everybody who does
+            not like individual workouts and wishes to have good time while
+            getting fit. Besides, at Steelworks gym the trainers really care
+            about the gym members and they are willing to help them whenever
+            they have a questions. But what makes Steelworks gym a fantastic
+            place is the atmosphere, everybody is very nice and welcome. I would
+            recommend Steelworks gym to everybody who would like to get results
+            and have fun at the same time."
+          </p>
+          <div className="flex xl:flex-row lg:flex-row xl:items-end lg:items-end sm:flex-col flex-col items-center mt-8">
+            <img
+              className="inline w-48 ml-4 rounded-full border-2 border-blue-500"
+              src={gabbyC}
+              alt="Avatar of Gabby C."
+            />
+            <span className="mb-4 "> - Gabby C.</span>
+          </div>
+        </div>
       </section>
       <img
         className="w-full"
@@ -123,10 +153,10 @@ function Home() {
         <h2 id="contact_section_heading" className="hidden">
           Contact
         </h2>
-        <div className="w-full h-full flex justify-center items-start mt-32 ">
+        <div className="w-full h-full flex flex-col justify-center items-center mt-8 ">
           <fieldset
             className={css`
-              width: 30%;
+              width: 33%;
               min-width: 295px;
               border-radius: 5px;
               border: #64748b solid 2px;
@@ -139,11 +169,12 @@ function Home() {
           >
             <form
               action="https://formspree.io/f/xqkjjegb"
-              className="w-2/3 h-2/3 flex flex-col z-50 mb-4"
+              className="w-4/5 h-3/5 flex flex-col z-50 pt-0 pb-0"
               method="POST"
+              target="_blank"
             >
-              <span className="text-white w-full mb-4">Contact</span>
-              <label htmlFor="email_field" className="text-white">
+              <span className="text-white w-full my-4 text-2xl">Contact</span>
+              <label htmlFor="email_field" className="text-white mb-1">
                 Email
               </label>
               <input
@@ -155,14 +186,14 @@ function Home() {
                 required
               />
 
-              <label htmlFor="message_field" className="text-white">
+              <label htmlFor="message_field" className="text-white mb-1">
                 Message
               </label>
               <textarea
                 id="message_field"
                 name="message_field"
                 className={`mb-4 rounded p-1 ${css`
-                  height: 200px;
+                  height: 300px;
                 `}`}
                 placeholder="Please type your message here"
                 required
@@ -182,6 +213,27 @@ function Home() {
                 Submit
               </Button>
             </form>
+            <div className="flex lg:flex-row md:flex-col sm:flex-col flex-col w-4/5 text-white font-extrabold text-xl mb-4">
+              <div className="mt-4 w-full">
+                <span className="mt-4 w-full">Address:</span>
+                <address className="mt-4 w-full">
+                  9 Moss Lane
+                  <br />
+                  Riverway
+                  <br />
+                  South Douglas Road
+                  <br />
+                  Cork
+                  <br />
+                  T12 K5FW
+                </address>
+              </div>
+              <div className="mt-4 w-full">
+                <span className=" mt-4 w-full">Tel:</span>
+                <br />
+                <p>+00353 021 1234567</p>
+              </div>
+            </div>
           </fieldset>
         </div>
       </section>
